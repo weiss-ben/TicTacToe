dad9513cb4bb52143500599ce1d35b6a16558c2a
weiss-ben <90002567+weiss-ben@users.noreply.github.com>
2021-10-10 04:20:41 +0300

    Added functionality for player to enter coordinates and a method to update the board state

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class TicTacToe {
-    public enum Pieces {X, O};
+    public enum Pieces {X, O,};
 
     //Displays the main menu
     public static void displayMenu() {
@@ -10,7 +10,7 @@ public class TicTacToe {
 
         System.out.println("1. Play");
         System.out.println("2. Game Stats");
-        System.out.println("3. Quit");
+        System.out.println("3. Quit\n");
 
         System.out.println("Please select a menu option: ");
     }
@@ -33,11 +33,22 @@ public class TicTacToe {
     }
 
     //Displays the game board in its current state
-    public static void displayBoard(int[][] spaces) {
+    public static void displayBoard(Pieces[][] spaces) {
         int i, j;   //index variables for looping over board
+        int[] boardIndicesLetters = {1, 2, 3};
 
         for(i = 0; i < spaces.length; ++i) {
             for(j = 0; j < spaces[i].length; ++j) {
+
+                //Format board position indices
+                if(i == 0 && j == 0) {
+                    System.out.println("  1 2 3");
+                }
+                if(j == 0) {
+                    System.out.print(boardIndicesLetters[i] + " ");
+                }
+
+                //Print X and 0
                 System.out.print(spaces[i][j]);
 
                 //Format the grid
@@ -47,7 +58,31 @@ public class TicTacToe {
 
                 //Printing line in between rows
                 if((j == 2) && (i < 2)) {
-                    System.out.println("\n-----");
+                    System.out.println("\n  -----");
+                }
+            }
+        }
+        System.out.println("\n");
+    }
+
+    //Gets values for the players move
+    public static int[] playerMove(Scanner input) {
+        int[] moveCoords = new int[2];
+
+        //Prompt for input
+        System.out.println("Please enter your move in the form: x y");
+        moveCoords[0] = input.nextInt();
+        moveCoords[1] = input.nextInt();
+
+        return moveCoords;
+    }
+
+    //Updates the board state with player move
+    public static void updateBoard(int[] move, Pieces[][] board) {
+        for(int i = 0; i < board.length; ++i) {
+            for(int j = 0; j < board[i].length; ++j) {
+                if((i == move[0]) && (j == move[1])) {
+                    board[i - 1][j - 1] = Pieces.X;
                 }
             }
         }
@@ -67,6 +102,7 @@ public class TicTacToe {
 
         boolean winCondition = false;
         int menuChoice;
+        int[] playerMove;
 
         //Get first player input
         displayMenu();
@@ -74,15 +110,13 @@ public class TicTacToe {
 
 
         //while(!winCondition) {
-            //display board
-            //get players move
-            //update board
-            //display the board
+            displayBoard(board);
+            playerMove = playerMove(input);
+            updateBoard(playerMove, board);
+            displayBoard(board);
             //check win condition
 
         //}
-
-        displayBoard(coordinates);
 
 
     }
